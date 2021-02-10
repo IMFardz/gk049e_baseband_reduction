0c8cbdfcb2a0ca9ab1f6b3261ac008f9dfb8a503
Fardin Syed <fardin.syed@mail.utoronto.ca>
2021-02-10 15:44:22 -0500

    Fixed typo

--- Fold_Arecibo.sh
+++ Fold_Arecibo.sh
@@ -11,10 +11,10 @@ export OMP_NUM_THREADS=32           # Number of cores in mussel. If using lobste
 
 source ~/.bashrc
 
-#python ar_fold.py gk049e_ar_no0004.m5b > /Users/fardinsyed/Desktop/PHY478/gk049e_baseband_reduction/logs/ar/no0004.log
+#python ar_fold.py gk049e_ar_no0004.m5b > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/ar/no0004.log
 
-python ar_fold.py gk049e_ar_no0005.m5b > /Users/fardinsyed/Desktop/PHY478/gk049e_baseband_reduction/logs/ar/no0005.log
+python ar_fold.py gk049e_ar_no0005.m5b > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/ar/no0005.log
 
-#python ar_fold.py gk049e_ar_no0023.m5b > /Users/fardinsyed/Desktop/PHY478/gk049e_baseband_reduction/logs/ar/no0023.log
+#python ar_fold.py gk049e_ar_no0023.m5b > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/ar/no0023.log
 
-#python ar_fold.py gk049e_ar_no0026.m5b > /Users/fardinsyed/Desktop/PHY478/gk049e_baseband_reduction/logs/ar/no0026.log
+#python ar_fold.py gk049e_ar_no0026.m5b > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/ar/no0026.log
