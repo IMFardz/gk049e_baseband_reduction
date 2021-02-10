b4d9a0bab5bc45591ca91a71fd84be260bb7e316
Fardin Syed <fardin.syed@mail.utoronto.ca>
2021-02-10 15:41:25 -0500

    Updated files to work on CITA machines

--- Fold_Arecibo.sh
+++ Fold_Arecibo.sh
@@ -5,16 +5,16 @@
 #SBATCH --job-name=ar_baseband_tasks
 #SBATCH --mail-type=ALL
 
-cd /scratch/p/pen/syedfard/B1133/gk049e/gk049e_baseband_reduction
+cd /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction
 
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_NUM_THREADS=32           # Number of cores in mussel. If using lobster, use 16 instead
 
 source ~/.bashrc
 
-python ar_fold.py gk049e_ar_no0004.m5b > /scratch/p/pen/syedfard/B1133/gk049e/gk049e_baseband_reduction/logs/ar/no0004.log
+#python ar_fold.py gk049e_ar_no0004.m5b > /Users/fardinsyed/Desktop/PHY478/gk049e_baseband_reduction/logs/ar/no0004.log
 
-python ar_fold.py gk049e_ar_no0005.m5b > /scratch/p/pen/syedfard/B1133/gk049e/gk049e_baseband_reduction/logs/ar/no0005.log
+python ar_fold.py gk049e_ar_no0005.m5b > /Users/fardinsyed/Desktop/PHY478/gk049e_baseband_reduction/logs/ar/no0005.log
 
-python ar_fold.py gk049e_ar_no0023.m5b > /scratch/p/pen/syedfard/B1133/gk049e/gk049e_baseband_reduction/logs/ar/no0023.log
+#python ar_fold.py gk049e_ar_no0023.m5b > /Users/fardinsyed/Desktop/PHY478/gk049e_baseband_reduction/logs/ar/no0023.log
 
-python ar_fold.py gk049e_ar_no0026.m5b > /scratch/p/pen/syedfard/B1133/gk049e/gk049e_baseband_reduction/logs/ar/no0026.log
+#python ar_fold.py gk049e_ar_no0026.m5b > /Users/fardinsyed/Desktop/PHY478/gk049e_baseband_reduction/logs/ar/no0026.log
--- ar_fold.py
+++ ar_fold.py
@@ -7,10 +7,10 @@ import folding_pipeline as sr
 from baseband_tasks.shaping import Reshape
 import traceback
 
-fdir = '/scratch/p/pen/syedfard/B1133/gk049e/e-1133/ar/'
-name = sys.argv[1]
+fdir = '/mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/baseband_data/ar/'
+fname = sys.argv[1]
 #fname = 'gk049e_ar_no0005.m5b'
-output_name = '/scratch/p/pen/syedfard/B1133/gk049e/numpy_arrays/ar' + fname[:-4]
+output_name = '/mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/numpy_arrays/ar/' + fname[:-4]
 print("Output File Name: {}".format(output_name))
 
 # Load Data
@@ -18,7 +18,7 @@ frequency = np.array([[332.00], [332.00]]) * u.MHz
 sideband = np.array([[-1, -1], [1, 1]])
 polarization = ['R', 'L']   # Right circular polarization & left circular polarization
 dispersion_measure = 4.84066 * u.pc / u.cm**3
-polyco_file = '/scratch/p/pen/syedfard/B1133/gk049e/polycos/ar/polyco_new.dat'
+polyco_file = '/mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/polycos/ar/polyco_new.dat'
 fullpol = False
 print("Parameters set")
 
