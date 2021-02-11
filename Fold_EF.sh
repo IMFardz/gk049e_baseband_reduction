fd929127d905f29ed6947525592d9f24419a3ca0
Fardin Syed <fardin.syed@mail.utoronto.ca>
2021-02-10 21:01:59 -0500

    Made eff script

--- Fold_EF.sh
+++ Fold_EF.sh
@@ -4,4 +4,4 @@ export OMP_NUM_THREADS=32           # Number of cores in mussel. If using lobste
 
 source ~/.bashrc
 
-python jb_fold.py gk049e_jb_no0005.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/jb/no0005.log
+python ef_fold.py gk049e_ef_no0005.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/ef/no0005.log
