4a775158cd83c7f3731f35c7291d6eda7979c5f8
Fardin Syed <fardin.syed@mail.utoronto.ca>
2021-02-11 23:49:08 -0500

    Updated bash script

--- Fold_JB.sh
+++ Fold_JB.sh
@@ -5,3 +5,15 @@ export OMP_NUM_THREADS=32           # Number of cores in mussel. If using lobste
 source ~/.bashrc
 
 python jb_fold.py gk049e_jb_no0005.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/jb/no0005.log
+
+python jb_fold.py gk049e_jb_no0001.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/jb/no0001.log
+
+python jb_fold.py gk049e_jb_no0004.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/jb/no0004.log
+
+python jb_fold.py gk049e_jb_no0023.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/jb/no0005.log
+
+python jb_fold.py gk049e_jb_no0026.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/jb/no0005.log
+
+
+
+
--- Fold_VLA.sh
+++ Fold_VLA.sh
@@ -5,3 +5,37 @@ export OMP_NUM_THREADS=32           # Number of cores in mussel. If using lobste
 source ~/.bashrc
 
 python yy_fold.py gk049e_Yy_scan51.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/yy/scan51.log
+
+python yy_fold.py gk049e_Yy_scan05.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/yy/scan05.log
+
+python yy_fold.py gk049e_Yy_scan11.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/yy/scan11.log
+
+python yy_fold.py gk049e_Yy_scan13.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/yy/scan13.log
+
+python yy_fold.py gk049e_Yy_scan15.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/yy/scan15.log
+
+python yy_fold.py gk049e_Yy_scan22.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/yy/scan22.log
+
+python yy_fold.py gk049e_Yy_scan24.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/yy/scan24.log
+
+python yy_fold.py gk049e_Yy_scan26.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/yy/scan26.log
+
+python yy_fold.py gk049e_Yy_scan33.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/yy/scan33.log
+
+python yy_fold.py gk049e_Yy_scan35.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/yy/scan35.log
+
+python yy_fold.py gk049e_Yy_scan42.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/yy/scan42.log
+
+python yy_fold.py gk049e_Yy_scan44.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/yy/scan44.log
+
+python yy_fold.py gk049e_Yy_scan53.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/yy/scan53.log
+
+python yy_fold.py gk049e_Yy_scan60.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/yy/scan60.log
+
+python yy_fold.py gk049e_Yy_scan62.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/yy/scan62.log
+
+python yy_fold.py gk049e_Yy_scan64.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/yy/scan64.log
+
+python yy_fold.py gk049e_Yy_scan66.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/yy/scan66.log
+
+python yy_fold.py gk049e_Yy_scan73.vdif > /mnt/scratch-lustre/fsyed/B1133+16/Analysis2020/gk049e/gk049e_baseband_reduction/logs/yy/scan73.log
